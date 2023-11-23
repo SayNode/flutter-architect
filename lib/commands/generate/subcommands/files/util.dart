21e0529dd54079d35fb0fc4855c7f37f56ee623f
Francesco Romeo <f.romeo.f@gmail.com>
2023-11-23 09:09:57 +0100

    updated as per change request

--- lib/commands/generate/subcommands/files/util.dart
+++ lib/commands/generate/subcommands/files/util.dart
@@ -7,28 +7,12 @@ bool getMaterialAppCalled = false;
 
 double getRelativeWidth(double width) {
   var screenSize = MediaQuery.of(Get.context!).size;
-  return screenSize.width * (width / 428);
+  return screenSize.width * (width / 428); // TODO: check this value with your current design
 }
 
 double getRelativeHeight(double height) {
   var screenSize = MediaQuery.of(Get.context!).size;
-  return screenSize.height * (height / 926);
-}
-
-//Checks if string is a valid address(42 characters, starts with 0x and is hex)
-bool isAddress(String address) {
-  if (address.length != 42) {
-    return false;
-  }
-  if (!address.startsWith("0x")) {
-    return false;
-  }
-  try {
-    int.parse(address.substring(2), radix: 16);
-  } catch (e) {
-    return false;
-  }
-  return true;
+  return screenSize.height * (height / 926); // TODO: check this value with your current design
 }
 
   """;
