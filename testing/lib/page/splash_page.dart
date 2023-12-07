0664d19003bd785d7cf7fc17365a8ab92c4409a3
Alcachofraz <75852333+Alcachofraz@users.noreply.github.com>
2023-12-07 17:48:49 +0000

    remove testign directory

--- testing/lib/page/splash_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
