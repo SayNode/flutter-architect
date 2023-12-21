f04fbb089e7393d746dc27eca3bdde88c62ec622
Alcachofraz <75852333+Alcachofraz@users.noreply.github.com>
2023-12-21 16:53:56 +0000

    fix

--- lib/commands/new/new.dart
+++ lib/commands/new/new.dart
@@ -149,9 +149,9 @@ class Creator extends Command {
         if (line.contains('flutter:') &&
             pubspec.contains('dev_dependencies:\n')) {
           pubspec += '  assets:\n';
-          pubspec += '    - asset/image\n';
-          pubspec += '    - asset/locale\n';
-          pubspec += '    - asset/video\n';
+          pubspec += '    - asset/image/\n';
+          pubspec += '    - asset/locale/\n';
+          pubspec += '    - asset/video/\n';
           pubspec += '\n';
         }
       }
