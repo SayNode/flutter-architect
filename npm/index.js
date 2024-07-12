5e4472d35bdec233a320ddef8f3345444d340bae
Alcachofraz <pedronuno2806@gmail.com>
2024-07-12 11:26:39 +0100

    org

--- npm/index.js
+++ npm/index.js
@@ -1,5 +1,5 @@
 function architect() {
-    return 'Architect is a CLI application and can\'t be used as a JS module'
+    return 'Architect is a CLI application and can\'t be used as a JS module.'
 }
 
 module.exports = architect
